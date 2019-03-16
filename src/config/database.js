7fb4de694a12f39fd1773e8c2d8bf8aa6991b6de
zsevic <zsevic95@gmail.com>
2019-03-16 18:48:18 +0100

    :zap: improve database config

---
+++ src/config/database.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+import constants from './constants'
+
+mongoose.Promise = global.Promise
+
+try {
+  mongoose.connect(
+    constants.MONGODB_URL,
+    { useNewUrlParser: true }
+  )
+} catch (err) {
+  mongoose.createConnection(constants.MONGODB_URL)
+}
+
+mongoose.connection
+  .once('open', () => {
+    console.log('Connection with database is established')
+  })
+  .on('error', e => {
+    throw e
+  })
--- src/index.js
+++ src/index.js
@@ -1,30 +1,12 @@
 import express from 'express'
 import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
-import constants from './constants'
+import './config/database'
 
 const app = express()
 const PORT = process.env.PORT || 8080
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-
-try {
-  mongoose.connect(
-    constants.MONGODB_URL,
-    { useNewUrlParser: true }
-  )
-} catch (err) {
-  mongoose.createConnection(constants.MONGODB_URL)
-}
-
-mongoose.connection
-  .once('open', () => {
-    console.log('Connection with database is established')
-  })
-  .on('error', e => {
-    throw e
-  })
 
 app.get('/', (req, res) => {
   res.end('Hello from express-starter')
