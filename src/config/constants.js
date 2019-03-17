2857de2a24009e18f0c10463575fe3ce2a332103
zsevic <zsevic95@gmail.com>
2019-03-17 23:21:47 +0100

    :rocket: setup Heroku configuration

--- src/config/constants.js
+++ src/config/constants.js
@@ -2,7 +2,7 @@ import 'dotenv/config'
 
 const config = {
   MONGODB_URL:
-    process.env.MONGODB_URL || 'mongodb://localhost:27017/express-starter-db'
+    process.env.MONGODB_URI || 'mongodb://localhost:27017/express-starter-db'
 }
 
 export default config
