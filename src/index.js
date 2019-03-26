553bc8a0bdc53ecf24ae832e525c965ef83ae391
zsevic <zsevic95@gmail.com>
2019-03-26 12:47:18 +0100

    :heavy_plus_sign: add morgan

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import morgan from 'morgan'
 import './config/database'
 
 const app = express()
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 8080
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
   res.end('Hello from express-starter')
