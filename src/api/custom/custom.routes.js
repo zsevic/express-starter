ddbbbb9a582ddf062aa4724a118ee24b167c775c
zsevic <zsevic95@gmail.com>
2019-11-17 23:24:27 +0100

    :zap: Add validation

--- src/api/custom/custom.routes.js
+++ src/api/custom/custom.routes.js
@@ -1,8 +1,11 @@
 import { Router } from 'express'
+import { createValidator } from 'express-joi-validation'
 import * as customController from './custom.controller'
+import { customQuery } from './custom.validation'
 
 const customRouter = Router()
+const validator = createValidator({})
 
-customRouter.get('/', customController.getCustomListHandler)
+customRouter.get('/', validator.query(customQuery), customController.getCustomListHandler)
 
 export default customRouter
---
+++ src/api/custom/custom.validation.js
@@ -0,0 +1,5 @@
+import Joi from '@hapi/joi'
+
+export const customQuery = Joi.object({
+  name: Joi.string().min(5).optional()
+})
