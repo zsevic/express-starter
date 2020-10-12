97e9ea2c466a079aca36c5369f7d0b78c873bd7c
zsevic <zsevic95@gmail.com>
2020-10-12 17:52:13 +0200

    Fix lint errors

--- src/api/error-handler.js
+++ src/api/error-handler.js
@@ -1,5 +1,6 @@
 import { isBoom } from '@hapi/boom';
 
+// eslint-disable-next-line no-unused-vars
 export function errorHandler(err, req, res, next) {
   if (isBoom(err)) {
     return res.status(err.output.statusCode).send(err.output.payload.message);
